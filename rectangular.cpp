64bacd05b5103513a1c43627cbd5a038e5b0e2d6
saadmansakib75 <saadmansakib175@gmail.com>
2025-03-17 14:56:18 +0600

    first commit

---
+++ rectangular.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+
+class rect {
+    private :
+       double length ;
+       double width ;
+    
+    public :
+        double area, perimeter ;
+
+        rect (double l, double w){
+            length = l ;
+            width = w ;
+        }
+
+    double getArea(){
+        area = length * width ;
+        return area ;
+    }  
+    
+    double getPerimeter(){
+        perimeter = 2 * (length + width) ;
+        return perimeter ;
+    }
+    
+    void printingAns(){
+        cout << "length = " << length << endl ;
+        cout << "width = " << width << endl ;
+        cout << "area = " << getArea() << endl ;
+        cout << "perimeter = " << getPerimeter() << endl ;
+    } 
+
+
+};
+
+
+int main(){
+    
+    double a , b ;
+    cout << "Enter length and width : " << endl ;
+    cin >> a >>  b ;
+
+    rect r1(a,b);
+    r1.printingAns();
+    return 0;
+}Object Oriented Proigraming
