53157c04f3c9b11587d07191fc65e22a1ec6583b
Md Saadman Sakib <162807194+saadmansakib75@users.noreply.github.com>
2025-03-17 23:15:25 +0600

    Update rectangular.cpp

--- rectangular.cpp
+++ rectangular.cpp
@@ -1,3 +1,6 @@
+/*Write a C++ program to create a class called Rectangle that has private member variables for
+length and width. Implement member functions to calculate the rectangle's area and perimeter.*/
+
 #include<iostream>
 using namespace std;
 
